27dc78dcca70763bfb8806edf775487b46d56a59
Rory Mitchell <r.a.mitchell.nz@gmail.com>
2019-12-27 18:48:13 -0800

    Use cub select if

--- include/shuffle/BijectiveFunctionScanShuffle.h
+++ include/shuffle/BijectiveFunctionScanShuffle.h
@@ -1,18 +1,40 @@
 #pragma once
 #include <thrust/device_vector.h>
 
-#include <thrust/copy.h>
-#include <thrust/gather.h>
-#include <thrust/scan.h>
-#include <thrust/transform.h>
-#include <cub/cub.cuh>
 #include "DefaultRandomGenerator.h"
 #include "shuffle/Shuffle.h"
+#include <cub/cub.cuh>
+#include <thrust/copy.h>
+#include <thrust/gather.h>
+#include <thrust/iterator/discard_iterator.h>
+#include <thrust/scan.h>
+#include <thrust/transform.h>
+
+
+struct IsValidFunctor
+{
+    int m;
+    __forceinline__ IsValidFunctor( int m ) : m( m )
+    {
+    }
+    __device__ __forceinline__ bool operator()( const uint64_t& x ) const
+    {
+        return x < m;
+    }
+};
 
 template <class BijectiveFunction, class ContainerType = thrust::device_vector<uint64_t>, class RandomGenerator = DefaultRandomGenerator>
 class BijectiveFunctionScanShuffle : public Shuffle<ContainerType, RandomGenerator>
 {
+    thrust::device_vector<uint8_t> temp_storage;
+    thrust::device_vector<uint64_t> gather_keys;
+
 public:
+    BijectiveFunctionScanShuffle()
+    {
+        temp_storage.resize( 1 << 16 );
+        gather_keys.resize( 1 << 27 + 1 );
+    }
     void shuffle( const ContainerType& in_container, ContainerType& out_container, uint64_t seed, uint64_t num ) override
     {
         assert( &in_container != &out_container );
@@ -23,38 +45,32 @@ public:
         uint64_t capacity = mapping_function.getMappingRange();
 
         thrust::counting_iterator<uint64_t> indexes( 0 );
-
-
-        /*
-        thrust::device_vector<uint64_t> mappings( capacity );
-        auto mapping_it = mappings.begin();
-        thrust::transform( indexes, indexes + capacity, mapping_it,
-                           [mapping_function] __host__ __device__( uint64_t val ) -> uint64_t {
-                               // Call mapping functions
-                               return mapping_function( val );
-                           } );
-
-        // Transform iterator for low memory footprint - worse performance
-        // auto mapping_it =
-        //     thrust::make_transform_iterator( indexes, [mapping_function] __host__ __device__( uint64_t val ) -> uint64_t {
-        //         // Call mapping functions
-        //         return mapping_function( val );
-        //     } );
-
-        auto is_valid_it =
-            thrust::make_transform_iterator( mapping_it, [num] __host__ __device__( uint64_t val ) -> bool {
-                // Check if mapping is valid
-                return val < num;
+        auto mapping_it =
+            thrust::make_transform_iterator( indexes, [mapping_function] __device__( uint64_t val ) -> uint64_t {
+                // Call mapping functions
+                return mapping_function( val );
             } );
-
-        thrust::device_vector<uint64_t> scan_buf( capacity );
-        thrust::exclusive_scan( is_valid_it, is_valid_it + capacity, scan_buf.begin() );
-        auto out_permutation_iterator =
-            thrust::make_permutation_iterator( out_container.begin(), scan_buf.begin() );
-
-        thrust::gather_if( mapping_it, mapping_it + capacity, is_valid_it, in_container.begin(),
-                           out_permutation_iterator );
-                           */
+        if( gather_keys.size() < in_container.size() )
+        {
+            gather_keys.resize( in_container.size() );
+        }
+        // Determine temporary device storage requirements
+        void* d_temp_storage = NULL;
+        size_t temp_storage_bytes = 0;
+        cub::DeviceSelect::If( d_temp_storage, temp_storage_bytes, mapping_it, gather_keys.begin(),
+                               thrust::discard_iterator<int>(), capacity,
+                               IsValidFunctor( in_container.size() ) );
+        // Allocate temporary storage
+        if( temp_storage.size() < temp_storage_bytes )
+        {
+            temp_storage.resize( temp_storage_bytes );
+        }
+        // Run selection
+        cub::DeviceSelect::If( reinterpret_cast<void*>( temp_storage.data().get() ), temp_storage_bytes,
+                               mapping_it, gather_keys.begin(), thrust::discard_iterator<int>(),
+                               capacity, IsValidFunctor( in_container.size() ) );
+        thrust::gather( gather_keys.begin(), gather_keys.begin() + in_container.size(),
+                        in_container.begin(), out_container.begin() );
     }
 
     bool supportsInPlace() override
