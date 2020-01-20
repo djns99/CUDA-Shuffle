2629cfface073c5b0239b599c13227f26a4f3e2e
Rory Mitchell <r.a.mitchell.nz@gmail.com>
2020-01-19 19:35:06 -0800

    Use std::shuffle for gather/scatter, increased max size of benchmarks

--- benchmarks/ShuffleBench.cu
+++ benchmarks/ShuffleBench.cu
@@ -21,90 +21,97 @@
 using DataType = uint64_t;
 
 template <class ShuffleFunction>
-static void benchmarkScatterGather(benchmark::State &state) {
-  ShuffleFunction shuffler;
-  using ContainerType = typename ShuffleFunction::container_type;
+static void benchmarkScatterGather( benchmark::State& state )
+{
+    ShuffleFunction shuffler;
+    using ContainerType = typename ShuffleFunction::container_type;
 
-  // Shuffle second param adds 0 or 1 to compare power of two (best case) vs.
-  // one above power of two (worst case)
-  const uint64_t num_to_shuffle = (uint64_t)state.range(1) + state.range(0);
+    // Shuffle second param adds 0 or 1 to compare power of two (best case) vs.
+    // one above power of two (worst case)
+    const uint64_t num_to_shuffle = (uint64_t)state.range( 1 ) + state.range( 0 );
 
-  ContainerType in_container(num_to_shuffle);
-  ContainerType out_container(num_to_shuffle);
+    ContainerType in_container( num_to_shuffle );
+    ContainerType out_container( num_to_shuffle );
 
-  ContainerType tmp(in_container.size());
-  FeistelBijectiveScanShuffle<ContainerType> temp_shuffler;
-  thrust::sequence(tmp.begin(), tmp.end());
+    using HostVector = typename std::vector<typename ContainerType::value_type>;
+    HostVector tmp( in_container.size() );
+    StdShuffle<HostVector> temp_shuffler;
+    thrust::sequence( tmp.begin(), tmp.end() );
 
-  int seed = 0;
-  for (auto _ : state) {
-    state.PauseTiming();
-    temp_shuffler(tmp, in_container, seed);
-    checkCudaError(cudaDeviceSynchronize());
-    state.ResumeTiming();
-    // Benchmarks raw gather speed of a random permutation
-    shuffler(in_container, out_container, seed);
-    checkCudaError(cudaDeviceSynchronize());
-    seed++;
-  }
+    int seed = 0;
+    for( auto _ : state )
+    {
+        state.PauseTiming();
+        temp_shuffler( tmp, tmp, seed );
+        thrust::copy( tmp.begin(), tmp.end(), in_container.begin() );
+        checkCudaError( cudaDeviceSynchronize() );
+        state.ResumeTiming();
+        // Benchmarks raw gather speed of a random permutation
+        shuffler( in_container, out_container, seed );
+        checkCudaError( cudaDeviceSynchronize() );
+        seed++;
+    }
 
-  state.SetItemsProcessed(state.iterations() * num_to_shuffle);
-  uint64_t log = std::log2(num_to_shuffle);
-  std::stringstream s;
-  s << "Shuffle 2^" << log;
-  if (state.range(1)) {
-    s << " + 1";
-  }
-  state.SetLabel(s.str());
+    state.SetItemsProcessed( state.iterations() * num_to_shuffle );
+    uint64_t log = std::log2( num_to_shuffle );
+    std::stringstream s;
+    s << "Shuffle 2^" << log;
+    if( state.range( 1 ) )
+    {
+        s << " + 1";
+    }
+    state.SetLabel( s.str() );
 }
 
 template <class ShuffleFunction>
-static void benchmarkFunction(benchmark::State &state) {
-  ShuffleFunction shuffler;
-  using ContainerType = typename ShuffleFunction::container_type;
+static void benchmarkFunction( benchmark::State& state )
+{
+    ShuffleFunction shuffler;
+    using ContainerType = typename ShuffleFunction::container_type;
 
-  // Shuffle second param adds 0 or 1 to compare power of two (best case) vs.
-  // one above power of two (worst case)
-  const uint64_t num_to_shuffle = (uint64_t)state.range(1) + state.range(0);
+    // Shuffle second param adds 0 or 1 to compare power of two (best case) vs.
+    // one above power of two (worst case)
+    const uint64_t num_to_shuffle = (uint64_t)state.range( 1 ) + state.range( 0 );
 
-  ContainerType in_container(num_to_shuffle);
-  ContainerType out_container(num_to_shuffle);
+    ContainerType in_container( num_to_shuffle );
+    ContainerType out_container( num_to_shuffle );
 
-  int seed = 0;
-  for (auto _ : state) {
-    shuffler(in_container, out_container, seed);
-    checkCudaError(cudaDeviceSynchronize());
-    seed++;
-  }
+    int seed = 0;
+    for( auto _ : state )
+    {
+        shuffler( in_container, out_container, seed );
+        checkCudaError( cudaDeviceSynchronize() );
+        seed++;
+    }
 
-  state.SetItemsProcessed(state.iterations() * num_to_shuffle);
-  uint64_t log = std::log2(num_to_shuffle);
-  std::stringstream s;
-  s << "Shuffle 2^" << log;
-  if (state.range(1)) {
-    s << " + 1";
-  }
-  state.SetLabel(s.str());
+    state.SetItemsProcessed( state.iterations() * num_to_shuffle );
+    uint64_t log = std::log2( num_to_shuffle );
+    std::stringstream s;
+    s << "Shuffle 2^" << log;
+    if( state.range( 1 ) )
+    {
+        s << " + 1";
+    }
+    state.SetLabel( s.str() );
 }
 
-static void argsGenerator(benchmark::internal::Benchmark *b) {
-  b->Ranges({{1 << 8, 1 << 27}, {0, 1}});
+static void argsGenerator( benchmark::internal::Benchmark* b )
+{
+    b->Ranges( { { 1 << 8, 1 << 29 }, { 0, 1 } } );
 }
 
-BENCHMARK_TEMPLATE(benchmarkFunction, MergeShuffle<std::vector<DataType>>)
-    ->Apply(argsGenerator);
-BENCHMARK_TEMPLATE(benchmarkFunction,
-                   RaoSandeliusShuffle<std::vector<DataType>>)
-    ->Apply(argsGenerator);
+BENCHMARK_TEMPLATE( benchmarkFunction, MergeShuffle<std::vector<DataType>> )
+    ->Apply( argsGenerator );
+BENCHMARK_TEMPLATE( benchmarkFunction, RaoSandeliusShuffle<std::vector<DataType>> )
+    ->Apply( argsGenerator );
 // BENCHMARK_TEMPLATE( benchmarkFunction,
 // FeistelBijectiveShuffle<thrust::device_vector<DataType>> )->Apply(
 // argsGenerator );
 // BENCHMARK_TEMPLATE( benchmarkFunction,
 // FeistelBijectiveSortShuffle<thrust::device_vector<DataType>> )
 //    ->Apply( argsGenerator );
-BENCHMARK_TEMPLATE(benchmarkFunction,
-                   FeistelBijectiveScanShuffle<thrust::device_vector<DataType>>)
-    ->Apply(argsGenerator);
+BENCHMARK_TEMPLATE( benchmarkFunction, FeistelBijectiveScanShuffle<thrust::device_vector<DataType>> )
+    ->Apply( argsGenerator );
 // BENCHMARK_TEMPLATE( benchmarkFunction,
 // SPNetworkBijectiveShuffle<thrust::device_vector<DataType>> )->Apply(
 // argsGenerator );
@@ -120,9 +127,8 @@ BENCHMARK_TEMPLATE(benchmarkFunction,
 // BENCHMARK_TEMPLATE( benchmarkFunction,
 // LCGBijectiveSortShuffle<thrust::device_vector<DataType>> )->Apply(
 // argsGenerator );
-BENCHMARK_TEMPLATE(benchmarkFunction,
-                   LCGBijectiveScanShuffle<thrust::device_vector<DataType>>)
-    ->Apply(argsGenerator);
+BENCHMARK_TEMPLATE( benchmarkFunction, LCGBijectiveScanShuffle<thrust::device_vector<DataType>> )
+    ->Apply( argsGenerator );
 // BENCHMARK_TEMPLATE( benchmarkFunction,
 // NoOpBijectiveShuffle<thrust::device_vector<DataType>> )->Apply( argsGenerator
 // );
@@ -132,18 +138,15 @@ BENCHMARK_TEMPLATE(benchmarkFunction,
 // BENCHMARK_TEMPLATE( benchmarkFunction,
 // NoOpBijectiveScanShuffle<thrust::device_vector<DataType>> )->Apply(
 // argsGenerator );
-BENCHMARK_TEMPLATE(benchmarkScatterGather,
-                   GatherShuffle<thrust::device_vector<DataType>>)
-    ->Apply(argsGenerator);
-BENCHMARK_TEMPLATE(benchmarkScatterGather,
-                   ScatterShuffle<thrust::device_vector<DataType>>)
-    ->Apply(argsGenerator);
+BENCHMARK_TEMPLATE( benchmarkScatterGather, GatherShuffle<thrust::device_vector<DataType>> )
+    ->Apply( argsGenerator );
+BENCHMARK_TEMPLATE( benchmarkScatterGather, ScatterShuffle<thrust::device_vector<DataType>> )
+    ->Apply( argsGenerator );
 // BENCHMARK_TEMPLATE( benchmarkFunction,
 // SortShuffle<thrust::device_vector<DataType>> )->Apply( argsGenerator );
-BENCHMARK_TEMPLATE(benchmarkFunction, StdShuffle<std::vector<DataType>>)
-    ->Apply(argsGenerator);
-BENCHMARK_TEMPLATE(benchmarkFunction, FisherYatesShuffle<std::vector<DataType>>)
-    ->Apply(argsGenerator);
+BENCHMARK_TEMPLATE( benchmarkFunction, StdShuffle<std::vector<DataType>> )->Apply( argsGenerator );
+BENCHMARK_TEMPLATE( benchmarkFunction, FisherYatesShuffle<std::vector<DataType>> )
+    ->Apply( argsGenerator );
 // Too slow
 // BENCHMARK_TEMPLATE( benchmarkFunction,
 // LubyRackoffBijectiveShuffle<thrust::device_vector<uint64_t>> )->Apply(
