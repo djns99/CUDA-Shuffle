cebf69b3fb9c7a1d9fd8f66723618519c57cb606
djns99 <40156487+djns99@users.noreply.github.com>
2020-12-17 14:31:01 +1300

    Testing improvements

---
+++ include/shuffle/ThrustShuffle.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "DefaultRandomGenerator.h"
+#include "shuffle/Shuffle.h"
+#include "ThrustInclude.h"
+#include <vector>
+
+template <class ContainerType = thrust::device_vector<uint64_t>, class RandomGenerator = DefaultRandomGenerator>
+class ThrustShuffle : public Shuffle<ContainerType, RandomGenerator>
+{
+public:
+    void shuffle( const ContainerType& in_container, ContainerType& out_container, uint64_t seed, uint64_t num ) override
+    {
+        RandomGenerator g( seed );
+        thrust::shuffle_copy( in_container.begin(), in_container.begin() + num, out_container.begin(), g );
+    }
+
+    bool supportsInPlace() const override
+    {
+        return false;
+    }
+};
