38d5c3fc4bffd60bd660ab2a6047c8d6e4365129
djns99 <40156487+djns99@users.noreply.github.com>
2021-01-05 16:42:31 +1300

    Added citations

--- test/ParameterQualityTests.cu
+++ test/ParameterQualityTests.cu
@@ -212,3 +212,38 @@ TYPED_TEST( ParameterQualityTests, PermutationLength )
         reportStats( d_p_score );
     }
 }
+
+TYPED_TEST( RandomnessTests, TurningPointCount )
+{
+    const uint64_t shuffle_size = 1e6;
+    const uint64_t num_samples = 50;
+
+    thrust::host_vector<uint64_t> input( shuffle_size );
+    thrust::sequence( input.begin(), input.end(), 0 );
+    thrust::host_vector<uint64_t> output( shuffle_size );
+
+    // Distribution expected according to
+    // Hombas, V. (2004). Turning Points in Random Permutations. Teaching Statistics, 26(1), 17-19.
+    const double expected = 2.0 * (double)( shuffle_size - 2 ) / 3.0;
+    const double stddev = sqrt( (double)( 16 * shuffle_size - 29 ) / 90.0 );
+
+    std::vector<double> p_scores( num_samples );
+
+    for( uint64_t i = 0; i < num_samples; i++ )
+    {
+        this->shuffle( input, output, i, shuffle_size );
+        uint64_t num_tp = 0;
+        for( uint64_t j = 1; j < shuffle_size - 1; j++ )
+            // Check for turning point (i.e. both neighbours bigger or smaller)
+            if( ( output[j - 1] < output[j] ) == ( output[j + 1] < output[j] ) )
+                num_tp++;
+
+        const double z_score = ( (double)num_tp - expected ) / stddev;
+        const double p_val = 1.0 - erf( abs( z_score ) / sqrt( 2.0 ) );
+        std::cout << p_val << ",";
+        p_scores.emplace_back( p_val );
+    }
+
+    std::cout << std::endl;
+    reportStats( p_scores );
+}
